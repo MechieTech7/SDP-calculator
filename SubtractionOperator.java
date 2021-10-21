b8904863d52281e5eb5d952ae1e773ee2be34a83
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-10-21 11:00:20 +0530

    Update SubtractionOperator.java

--- SubtractionOperator.java
+++ SubtractionOperator.java
@@ -1,7 +1,7 @@
 package com.sdp;
 public class SubtractOperator implements IOperator {
     @Override
-    public void executeOperation(int a, int b) {
-        System.out.println("SubtarctOperator result : " + (a - b));
+    public void executeOperation(int Number1, int Number2) {
+        System.out.println("SubtarctOperator result : " + (Number1 - Number2));
     }
 }
