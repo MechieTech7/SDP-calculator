e9026bbe2cb009ef4d892736280f524114dcd078
Lokesh Chandramurthy <79828815+MechieTech7@users.noreply.github.com>
2021-10-21 11:01:18 +0530

    Update AddOperator.java

--- AddOperator.java
+++ AddOperator.java
@@ -2,7 +2,7 @@ package com.sdp;
 public class AddOperator implements IOperator {
 
     @Override
-    public void executeOperation(int a, int b) {
-        System.out.println("AddOperator result : " + (a + b));
+    public void executeOperation(int Number1, int Number2) {
+        System.out.println("AddOperator result : " + (Number1 + Number2));
     }
 }
